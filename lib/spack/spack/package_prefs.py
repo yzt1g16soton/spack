318c74a3e60db4e6098d3655dee6949c629dda56
Gregory Becker <becker33@llnl.gov>
2020-08-25 12:08:00 -0700

    respect spec buildable that overrides virtual buildable

--- lib/spack/spack/package_prefs.py
+++ lib/spack/spack/package_prefs.py
@@ -189,18 +189,17 @@ def spec_externals(spec):
 
 def is_spec_buildable(spec):
     """Return true if the spec pkgspec is configured as buildable"""
-
     allpkgs = spack.config.get('packages')
     all_buildable = allpkgs.get('all', {}).get('buildable', True)
+    so_far = all_buildable  # the default "so far"
 
-    # Get the list of names for which all_buildable is overridden
-    reverse = [name for name, entry in allpkgs.items()
-               if entry.get('buildable', all_buildable) != all_buildable]
-    # Does this spec override all_buildable
-    spec_reversed = (spec.name in reverse or
-                     any(spec.package.provides(name) for name in reverse))
-    return not all_buildable if spec_reversed else all_buildable
+    # check whether any providers for this package override the default
+    if any(spec.package.provides(name) and entry.get('buildable', so_far) != so_far
+           for name, entry in allpkgs.items()):
+        so_far = not so_far
 
+    spec_buildable = allpkgs.get(spec.name, {}).get('buildable', so_far)
+    return spec_buildable
 
 def get_package_dir_permissions(spec):
     """Return the permissions configured for the spec.
