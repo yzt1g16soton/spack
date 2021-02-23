937b547b9869094c21f03635e8a1b7a7d2b56ec6
Harmen Stoppels <harmenstoppels@gmail.com>
2021-02-23 09:29:05 +0100

    Revert "bugfix: add build deps to 'full hash' (#21735)"
    This reverts commit de5a396ecb28bbe284fcca82dd4c773806a703a8.

--- lib/spack/spack/directory_layout.py
+++ lib/spack/spack/directory_layout.py
@@ -15,7 +15,6 @@ import ruamel.yaml as yaml
 from llnl.util.filesystem import mkdirp
 
 import spack.config
-import spack.hash_types as ht
 import spack.spec
 from spack.error import SpackError
 
@@ -243,9 +242,7 @@ class YamlDirectoryLayout(DirectoryLayout):
         """Write a spec out to a file."""
         _check_concrete(spec)
         with open(path, 'w') as f:
-            # The hash the the projection is the DAG hash but we write out the
-            # full provenance by full hash so it's availabe if we want it later
-            spec.to_yaml(f, hash=ht.full_hash)
+            spec.to_yaml(f)
 
     def read_spec(self, path):
         """Read the contents of a file and parse them as a spec"""
